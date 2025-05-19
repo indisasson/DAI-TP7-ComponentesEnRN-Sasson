e47a711f58f7cde3a9b37ce39043eda5d4c2d74a
Android Studio <devandroid@ort>
2025-05-19 12:14:53 -0300

    avances

--- DAI-TP7-SASSON/App.js
+++ DAI-TP7-SASSON/App.js
@@ -1,14 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground, SafeAreaView } from 'react-native';
 import Card from './components/Card';
+
+
+const imgaFondo =  {uri: 'https://as1.ftcdn.net/jpg/03/56/11/44/1000_F_356114402_KnAV2au0F2RnBTlTweaEus3royONjGAt.jpg'}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Card/>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={{flex:1}}>
+        <ImageBackground source={imgaFondo}  resizeMode='center' style={styles.ImagenFondo}> 
+        <View style={styles.container}>
+          <Card/>
+        </View>  
+      </ImageBackground>
+    </SafeAreaView>
+    
+    
   );
 }
 
@@ -19,4 +26,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  ImagenFondo:{
+    flex:1,
+    justifyContent: 'center',
+  },
+ 
 });
--- DAI-TP7-SASSON/components/Card.js
+++ DAI-TP7-SASSON/components/Card.js
@@ -1,20 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Image, TouchableOpacity, TextInput, Pressable } from 'react-native';
 
 const imga =  {uri: 'https://img.europapress.es/fotoweb/fotonoticia_20150331134913-15031252329_600.jpg'}
 
+
 export default function Card() {
+    const [colorBoton, setColorBoton] = useState("#BBD2EC") // pasar el use state a uno de los botones para que funcione la function del pressable
+
     return(
         <>
             <View style={style.contenedor}>
                 <StatusBar style={'light'}/>
                 <Image source={imga}  style={style.Image}/>
-                <Text style={style.nombre}>Adrián Fernendez</Text>
+                <Text style={style.nombre}>Adrián Fernández</Text>
+                <TouchableOpacity style={style.boton}><Text style={style.textoBoton}>Contactar</Text></TouchableOpacity>
+                <TouchableOpacity style={style.boton2}><Text style={style.textoBoton2}>Ver Portfolio</Text></TouchableOpacity>
+                <TextInput value="  Escribi algo" keyboardType='number-pad' style={style.Input}/>
+                <Pressable onPressIn={CambiarFondoBoton()} style={style.boton3}><Text>Ver Perfil</Text></Pressable>
                 
             </View>
         </>
     )
 };
+
+function CambiarFondoBoton(){
+    setColorBoton ("red")
+}
 
 const style = StyleSheet.create({
     contenedor: {
@@ -35,5 +47,46 @@ const style = StyleSheet.create({
         textAlign: 'center',
         marginTop: 10,
         fontSize: 30
+      },
+      boton: {
+        width: 80,
+        height: "auto",
+        backgroundColor: "#BBD2EC",
+        textAlign: "center",
+        padding: 3,
+        borderRadius: 8,
+        marginLeft: 70,
+        marginTop: 10
+      },
+      textoBoton: {
+        marginLeft: 5
+      },
+      boton2: {
+        width: 100,
+        height: "auto",
+        backgroundColor: "#BBD2EC",
+        textAlign: "center",
+        padding: 3,
+        borderRadius: 8,
+        marginLeft: 60,
+        marginTop: 10
+      },
+      textoBoton2: {
+        marginLeft: 8
+      },
+      Input:{
+        backgroundColor: "white",
+        borderRadius: 10,
+        marginTop: 10
+      },
+      boton3: {
+        width: 110,
+        height: "auto",
+        backgroundColor: "#BBD2EC",
+        textAlign: "center",
+        padding: 3,
+        borderRadius: 8,
+        marginLeft: 60,
+        marginTop: 10
       }
   });
